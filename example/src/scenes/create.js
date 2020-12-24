c10d4498a57acf30443feb1a50ecb6b4626ecb8f
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-24 13:02:44 -0500

    draw circle when ON_BUTTON_CLICK event received

--- example/src/scenes/create.js
+++ example/src/scenes/create.js
@@ -1,8 +1,12 @@
+import Phaser from 'phaser'
 import events from '../events'
 
 export default function create() {
-  // do create stuff
   this.game.events.on(events.ON_BUTTON_CLICK, (event) => {
     console.log(event)
+    const { width, height } = this.game.config
+    const color = new Phaser.Display.Color().random().color
+    const radius = Phaser.Math.Between(0, Math.max(width, height) / 2)
+    this.add.circle(width / 2, height / 2, radius, color)
   })
 }

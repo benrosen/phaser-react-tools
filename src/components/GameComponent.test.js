b1c7bea194a9485d7f242c3808bd72319ea739f5
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-27 22:04:27 -0500

    create test files with failing tests

---
+++ src/components/GameComponent.test.js
@@ -0,0 +1,5 @@
+import GameComponent from './GameComponent'
+
+test('should fail', () => {
+  expect(true).toBe(false)
+})
---
+++ src/hooks/useEventListener.test.js
@@ -0,0 +1,5 @@
+import useEventEmitter from './useEventListener'
+
+test('should fail', () => {
+  expect(true).toBe(false)
+})

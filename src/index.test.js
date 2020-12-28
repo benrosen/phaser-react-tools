0dac721303c9037d941ea94c1e93fd0875a6b0e6
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-27 22:38:59 -0500

    add failing test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,12 @@
-import { ExampleComponent } from '.'
+export {
+  GameComponent,
+  GameContext,
+  GameProvider,
+  useEventEmitter,
+  useEventListener,
+  usePhaser
+} from './index.js'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
+test('should fail', () => {
+  expect(true).toBe(false)
 })

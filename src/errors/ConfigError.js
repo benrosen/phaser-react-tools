522917767b1f85000f66c569de948b5aed0215ae
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-28 22:30:34 -0500

    create ConfigError and tests

---
+++ src/errors/ConfigError.js
@@ -0,0 +1,41 @@
+import InvalidPropertyNameError from './InvalidPropertyNameError'
+import MissingArgumentError from './MissingArgumentError'
+
+/**
+ * An error that occurs when a protected value on the Phaser config object is set incorrectly.
+ */
+export default class ConfigError extends TypeError {
+  /**
+   * Creates an instance representing an error that occurs when a protected value on the Phaser config object is set incorrectly.
+   *
+   * @param {Object} config The Phaser config object whose property value is set incorrectly.
+   * @param {*} propertyName The name of the property whose value is set incorrectly.
+   */
+  constructor(config, propertyName) {
+    if (!config) {
+      throw new MissingArgumentError(
+        'You must provide the Phaser config object whose property value is set incorrectly when attempting to create a ConfigError.'
+      )
+    }
+
+    if (!propertyName) {
+      throw new MissingArgumentError(
+        'You must provide the name of the property whose value is set incorrectly when attempting to create a ConfigError.'
+      )
+    }
+
+    const value = config[propertyName]
+
+    if (value === undefined) {
+      throw new InvalidPropertyNameError(
+        `The Phaser config object does not have a property named ${propertyName}.`
+      )
+    }
+
+    super(
+      `phaser-react-tools sets the ${propertyName} property of the Phaser config object internally. As such, the user-provided value of ${value} will be overridden.`
+    )
+
+    Error.captureStackTrace(this, ConfigError)
+  }
+}
---
+++ src/errors/ConfigError.test.js
@@ -0,0 +1,57 @@
+import ConfigError from './ConfigError'
+import InvalidPropertyNameError from './InvalidPropertyNameError'
+import MissingArgumentError from './MissingArgumentError'
+
+describe('The ConfigError', () => {
+  it('should throw a MissingArgumentError if the Phaser config object whose property value is set incorrectly is not provided.', () => {
+    expect(() => {
+      const config = undefined
+      const propertyName = 'title'
+      new ConfigError(config, propertyName)
+    }).toThrow(MissingArgumentError)
+  })
+
+  it('should throw a MissingArgumentError if the name of the property whose value is set incorrectly is not provided.', () => {
+    expect(() => {
+      const config = { title: 'My Phaser Game' }
+      const propertyName = undefined
+      new ConfigError(config, propertyName)
+    }).toThrow(MissingArgumentError)
+  })
+
+  it('should throw an InvalidPropertyNameError if the property is not defined on the Phaser config object.', () => {
+    expect(() => {
+      const config = { title: 'My Phaser Game' }
+      const propertyName = 'asdf'
+      new ConfigError(config, propertyName)
+    }).toThrow(InvalidPropertyNameError)
+  })
+
+  it('should be throwable.', () => {
+    expect(() => {
+      throw new ConfigError({ title: 'My Phaser Game' }, 'title')
+    }).toThrow(ConfigError)
+  })
+
+  describe('message', () => {
+    const config = { title: 'My Phaser Game' }
+    const propertyName = 'title'
+    const configError = new ConfigError(config, propertyName)
+    const message = configError.message
+
+    it('should be defined.', () => {
+      expect(message).toBeDefined()
+    })
+
+    it('should contain the name of the property whose value is set incorrectly.', () => {
+      const expression = new RegExp(propertyName)
+      expect(message).toMatch(expression)
+    })
+
+    it('should contain the value of the property whose value is set incorrectly.', () => {
+      const value = config[propertyName]
+      const expression = new RegExp(value)
+      expect(message).toMatch(expression)
+    })
+  })
+})

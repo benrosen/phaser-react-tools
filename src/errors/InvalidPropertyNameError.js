780af37d1713398833f557a0103b89254f923a09
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-28 21:55:04 -0500

    create InvalidPropertyNameError and tests

---
+++ src/errors/InvalidPropertyNameError.js
@@ -0,0 +1,14 @@
+/**
+ * An error that occurs when an invalid property name is provided.
+ */
+export default class InvalidPropertyNameError extends TypeError {
+  /**
+   * Creates an instance representing an error that occurs when an invalid property name is provided.
+   *
+   * @param  {...any} [args] The error arguments.
+   */
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this, InvalidPropertyNameError)
+  }
+}

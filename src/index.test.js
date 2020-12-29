5e6c200e28249ba32b464a266b023c464d0b925b
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-29 10:20:31 -0500

    edit description and assertion text

--- src/index.test.js
+++ src/index.test.js
@@ -7,28 +7,28 @@ import {
   usePhaser
 } from './index.js'
 
-describe('phaser-react-tools', () => {
-  it('should export GameComponent', () => {
+describe('The phaser-react-tools module', () => {
+  it('should export GameComponent.', () => {
     expect(GameComponent).toBeTruthy()
   })
 
-  it('should export GameContext', () => {
+  it('should export GameContext.', () => {
     expect(GameContext).toBeTruthy()
   })
 
-  it('should export GameProvider', () => {
+  it('should export GameProvider.', () => {
     expect(GameProvider).toBeTruthy()
   })
 
-  it('should export useEventEmitter', () => {
+  it('should export useEventEmitter.', () => {
     expect(useEventEmitter).toBeTruthy()
   })
 
-  it('should export useEventListener', () => {
+  it('should export useEventListener.', () => {
     expect(useEventListener).toBeTruthy()
   })
 
-  it('should export usePhaser', () => {
+  it('should export usePhaser.', () => {
     expect(usePhaser).toBeTruthy()
   })
 })
